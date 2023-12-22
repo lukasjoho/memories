797bad8958d7f86666e6f459d3aeaf7f68c90841
lukasjoho <social@lukashoppe.com>
2023-12-22 15:35:54 +0100

    fix: change metadata location

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import VerticalSwiper from '@/components/VerticalSwiper';
 import prisma from '@/lib/prisma';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Kobkhunkrub',
+  description: 'Replaying a thai-german friendship.',
+};
 
 export default async function Home() {
   const memories = await prisma.memory.findMany({
