495cea46b8068e4ebd0926ddf58789faab0a1209
lukasjoho <social@lukashoppe.com>
2023-12-22 15:09:11 +0100

    feat: add optimizedimage

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -8,6 +8,7 @@ import { cn } from '@/lib/utils';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { Keyboard, Mousewheel } from 'swiper/modules';
+import OptimizedImage from './OptimizedImage';
 
 interface GalleryProps {
   images: string[];
@@ -35,28 +36,31 @@ const Gallery = ({ images }: GalleryProps) => {
       centeredSlides={true}
     >
       {images.map((image, index) => (
-        <SwiperSlide style={{ width: 'auto' }} key={index}>
+        <SwiperSlide
+          style={{
+            width: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          key={index}
+        >
           {({ isActive }) => (
-            // <div
-            //   className={cn(
-            //     'h-full opacity-30 scale-90 transition duration-300 rounded-3xl overflow-hidden w-auto',
-            //     isActive && 'opacity-100 scale-100'
-            //   )}
-            // >
-            <img
-              src={image}
-              alt=""
+            <div
               className={cn(
-                'w-full h-full rounded-3xl overflow-hidden scale-90 opacity-30 transition duration-300',
+                'h-full rounded-3xl overflow-hidden scale-90 opacity-30 transition duration-300 max-h-[260px] md:max-h-[1400px]',
                 isActive && 'opacity-100 scale-100'
               )}
-              style={{
-                objectFit: 'contain',
-                maxWidth: '100%',
-                maxHeight: '100%',
-              }}
-            />
-            // </div>
+            >
+              <OptimizedImage
+                steps={[400, 800, 1200]}
+                style={{
+                  objectFit: 'contain',
+                }}
+                src={image}
+              />
+            </div>
           )}
         </SwiperSlide>
       ))}
