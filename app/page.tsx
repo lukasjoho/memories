2d83a4a15891df37356340bae670ff90be1f7cc8
lukasjoho <social@lukashoppe.com>
2023-12-22 15:55:13 +0100

    fix: add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,28 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Kobkhunkrub',
-  description: 'Replaying a thai-german friendship.',
+  description: 'A thai-german friendship.',
+  openGraph: {
+    images: [
+      {
+        url: 'https://res.cloudinary.com/du3mz9iny/image/upload/v1703256737/ogimage_ldnk3g.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Kobkhunkrub',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: [
+      {
+        url: 'https://res.cloudinary.com/du3mz9iny/image/upload/v1703256737/ogimage_ldnk3g.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Kobkhunkrub',
+      },
+    ],
+  },
 };
 
 export default async function Home() {
